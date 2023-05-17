af5fac4c2846a17f3c2809625f9be872555b467b
Night2323 <denessiposszabo@gmail.com>
2023-05-17 21:03:58 +0200

    töri 2.0

---
+++ css/JavaScript/töri.js
@@ -0,0 +1,132 @@
+function load() {
+    var selected = document.getElementById("pdfselector").value
+    if (selected == "1") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj/1.Az_őskor.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "2") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj/2.A polisz születése.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "3") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj/3.Az_athéni_demokrácia.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "4") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj/4.görög_perzsa.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "5") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj/5.görög_istenek.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "6") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj/6.Az_okori_Roma-Roma_alapitasatol_a_koztarsasagig.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "7") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj/7.Az_okori_Roma-A koztarsasag kora.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "8") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj/8.Az_okori_Roma-A koztarsasag kora-A_koztarsasag_valsaga.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "9") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj/9.Az_okori_Roma-A koztarsasag kora-A_polgarhaboruk_kora.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "10") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj/10.Az_okori_Roma-A koztarsasag kora-PUN_HABORUK.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "11") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj/11.A kereszténység születése és egyházzá szerveződése.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "12") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj/12.A nagy népvándorlás.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "13") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj/13.Az_iszlam.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "14") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj/14.Kalandozasok_kora.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "15") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj/15.Geza_es_Szent_Istvan.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "16") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj/16.A_Magyar_Kiralysag_elso_evszazada_Szent_Istvantol_Elso_Belaig.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "17") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj/17.A_Magyar_Kiralysag_elso_evszazada_Salamontol_Kalmanig.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "18") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj/18.III_Bela.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "19") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj/19.II_Andras_es_az_aranybulla.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "20") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj/20.I_Karoly_a_kiralyi_hatalom_helyreallitoja.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "21") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj2/1.Foldrajzi_felfedezesek.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "22") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj2/2.Reformacio.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "23") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj2/3.A Habsburgok felemelkedése és a katolikus megújulás.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "24") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj2/4.A franciák évszázada.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "25") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj2/5.Az_angol_polgári_forradalom.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "26") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj2/6.A_Jagello_kor001.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "27") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj2/7.A mohácsi csata001.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "28") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj2/8.A Várháborúk - a királyi Magyarország és a hódoltság (1).pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "29") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj2/9.A 15 eves haboru.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "30") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj2/11.A kiralyi_MO_a_17_szazadban.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "31") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj2/12.A torok kiuzese001.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "32") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj2/13.Rakoczi_szabadsagharc001.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "33") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj2/14.Maria_Terezia_es_II. Jozsef uralkodasa.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "34") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj2/15.Felvilagosodas-1.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "35") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj2/16.A-nagy-francia-forradalom-2.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "36") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj2/17.Napoleon_uralma_I_RESZ_001.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "37") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj2/18.Napoleon_uralma_II_RESZ_002.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "38") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj2/19.A reformkor előzményei.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "39") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj2/20.Reform_kor_Szechenyi_es_Kossuth002.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "40") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj2/21.Forradalom_-_1848március_15.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "41") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj2/22.Az_1848-49-es_szabadságharc.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "42") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj2/23.Az-ipari-forradalom-újabb-hulláma.pdf" frameborder="0"></iframe></div>'
+    }
+    else if (selected == "43") {
+        document.getElementById("pdfek").innerHTML = '<div class="keret"><iframe src="Töri/TöriÚj2/24.Nepesedesi_folyamatok_polgari_allam.pdf" frameborder="0"></iframe></div>'
+    }
+}
