9f265d07a1691e5cb38441cbd105bdc83ab379bb
White_Wolf55 <85016343+Asgardean@users.noreply.github.com>
2023-05-30 22:43:50 +0200

    Search.js 1.1

--- css/JavaScript/search.js
+++ css/JavaScript/search.js
@@ -1,12 +1,10 @@
 function Search(){
-    if("töri" in document.baseURI){
+    if(document.baseURI.includes("t%C3%B6ri.html")){
         var all = document.getElementById("pdfselector").innerHTML;
         var query = document.getElementById("keres").value;
-        if(query in all){
-            alert("Success")
-        }
-        else{
-            alert("Nincs találat")
+        const Arr = Array.from(all.matchAll(query))
+        if(Arr.length <= 10){
+            alert(Arr)
         }
     }
 }
