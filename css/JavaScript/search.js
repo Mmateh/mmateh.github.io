8ebbf8ba5a8baaaced336af6a14ff4a311362843
White_Wolf55 <85016343+Asgardean@users.noreply.github.com>
2023-06-03 00:31:25 +0200

    Search 1.2

--- css/JavaScript/search.js
+++ css/JavaScript/search.js
@@ -2,9 +2,26 @@ function Search(){
     if(document.baseURI.includes("t%C3%B6ri.html")){
         var all = document.getElementById("pdfselector").innerHTML;
         var query = document.getElementById("keres").value;
-        const Arr = Array.from(all.matchAll(query))
-        if(Arr.length <= 10){
-            alert(Arr)
+        const matchit = Array.from(all.matchAll(query));
+        if(matchit.length <= 10){
+            const Arr = Array.from(all.matchAll("<option"));
+            console.log(Arr)
+            var many = []
+            var count = 0
+            var item = NaN
+            var name = NaN
+            var items = NaN
+            for(let cunt=1;cunt<=Arr.length;cunt++){
+                console.log(cunt)
+                items = all.slice(all.indexOf("<option", count)+7, all.indexOf("/option>", count))
+                count = all.indexOf("</option>", count)+9
+                console.log(count)
+                item = items.slice(items.indexOf('value="')+7, items.indexOf('">', items.indexOf('value="')+6)).toString()
+                name = items.slice(items.indexOf('>'), items.indexOf('<')).toString()
+                console.log(item)
+                console.log(name)
+                many.push({name:item})
+        }
         }
     }
 }
