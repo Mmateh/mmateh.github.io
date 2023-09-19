3a6139dd828368b45d579e2fd8d6a16dbb373add
White_Wolf55 <85016343+Asgardean@users.noreply.github.com>
2023-09-19 20:30:50 +0200

    fizika kereső kész

--- css/JavaScript/search.js
+++ css/JavaScript/search.js
@@ -44,4 +44,37 @@ function Search(){
         }
         }
     }
+    if(document.baseURI.includes("fizika.html")){
+        var all = document.getElementById("items").innerHTML;
+        var query = document.getElementById("keres").value;
+        document.getElementById("inner").innerHTML = ""
+        const matchit = Array.from(all.matchAll(query));
+        if(matchit.length <= 7){
+            const Arr = Array.from(all.matchAll('href="'));
+            var many = [];
+            var result = [];
+            var item = NaN;
+            var name = NaN;
+            var items = NaN;
+            var count = 0;
+            for(let cunt=1;cunt<=Arr.length;cunt++){
+                items = all.slice(all.indexOf('href=', count)+5, all.indexOf("/a>", count)+3);
+                count = all.indexOf("/a>", count)+3;
+                item = items.slice(items.indexOf('"')+1, items.indexOf('"', items.indexOf('"')+1)).toString();
+                name = items.slice(items.indexOf('>')+1, items.indexOf('<')).toString();
+                many.push({"name":name, "link":item});
+            }
+            for(let cunt=0;cunt<many.length;cunt++){
+                if(many[cunt].name.includes(query)){
+                    result.push({"name":many[cunt].name, "link":many[cunt].link});
+                    console.log(result);
+                }
+            }
+            for(let cunt=0;cunt<result.length;cunt++){
+                var out = document.getElementById("inner").innerHTML;
+                out = out+'<li><a class="dropdown-item" href="'+result[cunt].link+'"+>'+result[cunt].name+'</a></li>'
+                document.getElementById("inner").innerHTML = out;
+            }
+        }
+    }
 }
